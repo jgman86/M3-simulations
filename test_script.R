05da6e2b8192e70505ea6c1c11db80c5b398b9d7
Jan Göttmann <jan@debian-BULLSEYE-live-builder-AMD64>
2022-07-29 13:47:42 +0200

    actual testing script

--- test_script.R
+++ test_script.R
@@ -9,10 +9,10 @@ nRetrievals <- 500
 minFT <- 0.5
 maxFT <- 1.5
 nFT <- c(2,4) # 2,4,10 Conditions between 0.2 and 2
-SampleSize <- 100
-con_nFT =5
+SampleSize <- 10
+con_nFT =2
 conFT <- seq(from = minFT, to = maxFT, length.out = con_nFT) # eventuell log scale 0.2 0.8 2.4 oder so?
-conFT <- c(0,0.2,0.4,0.8,1.6)
+#conFT <- c(0,0.2,0.4,0.8,1.6)
 # Set Range for Parameter Means
 range_muC <- c(1,100)
 range_muA <- c(0,0.5)
@@ -40,7 +40,6 @@ Mean_Rpar <- 5
 Mean_Fpar <-  0.5
 log_mu_f <- log(Mean_Fpar/(1-Mean_Fpar))
 Mean_bpar <- 0.1
-
 
 
 # Make Vector with Hyper Pars
@@ -117,8 +116,8 @@ stan1.dat <- list(count = data[,4:8],
                  Con = length(unique(data[,"Freetime"])),
                  Freetime = unique(data[,"Freetime"]),
                  retrievals = 1000,
-                 scale_b = 0.1)
-                  #f = .5)
+                 scale_b = 0.1,
+                 parms=parms)
 
 
 stan2.dat <- list(count = data[,4:8], 
@@ -131,72 +130,31 @@ stan2.dat <- list(count = data[,4:8],
                   f=0.5,
                   scale_b = 0.1)
 
-stan_fit <- function(mod, dat){
-  
-  #set_cmdstan_path(path="C:/Coding/cmdstan-2.30.0/")
-  
-  
-  init_univ <- function()
-  {
-    list(c=cbind(runif(stan2.dat$N,1,10)),
-         a=cbind(runif(stan2.dat$N,1,10)),
-         EE=cbind(runif(stan2.dat$N,1,10)),
-         r=cbind(runif(stan2.dat$N,1,10)),
-         mu_c=cbind(runif(stan2.dat$N,2,10)),
-         mu_a=cbind(runif(stan2.dat$N,2,10)),
-         #logMu_f=cbind(runif(stan2.dat$N,-1,1)),
-         mu_e =cbind(runif(stan2.dat$N,2,10)),
-         mu_r=cbind(runif(stan2.dat$N,2,10)))
-    
-  }
-  
-  
-  # Stan is noisy, so tell it to be more quiet()
-  M3 <-  mod$sample(dat,
-                          refresh = 100,
-                          chains = 4,
-                          parallel_chains=4, 
-                          iter_warmup=500,
-                          iter_sampling=1000,
-                          adapt_delta=.99,
-                          max_treedepth=15,
-                          init = init_univ,
-                          show_messages = F)
-  
-  M3_hyper <- M3$summary(c("hyper_pars","mu_f"), mean, Mode,sd,rhat,HDInterval::hdi)
-  M3_subj <- M3$summary(c("subj_pars"), mean, Mode,sd,rhat,HDInterval::hdi)
-  M3_f <- M3$summary("f", mean, Mode,sd,rhat,HDInterval::hdi)
-  M3_count_rep <-M3$summary("count_rep",mean)
-  M3_est_cor <- M3$summary("cor_mat_lower_tri",mean)
-  
-  
-  M3_sum <- list(M3_hyper,M3_subj,M3_f,M3_count_rep)
-  
-  M3_sum
-}
 
 
-results <- stan_fit(mod3_norm,dat = stan1.dat)
+
+M3 <- stan_fit(mod3_norm,dat = stan1.dat)
+
 
 # summarise results
 
-hyper <- results[[1]] %>% pivot_wider(.,names_from = "variable",values_from = c("mean","Mode","sd","rhat","upper","lower")) 
+hyper <- M3[[1]] %>% pivot_wider(.,names_from = "variable",values_from = c("mean","Mode","sd","rhat","upper","lower")) 
 
-subj <- results[[2]]  %>% mutate(variable = str_remove_all(variable, "subj_pars")) %>% 
+subj <- M3[[2]]  %>% mutate(variable = str_remove_all(variable, "subj_pars")) %>% 
   separate(col = variable,into = c("theta","ID"),sep = ",")  %>% 
   mutate(ID = str_remove(ID,pattern = "]"), theta = case_when(theta == "[1" ~ "c", theta == "[2" ~ "a",
                                                               theta == "[3" ~ "log_f",
                                                               theta == "[4" ~ "EE",
                                                               theta== "[5" ~ "r")) %>%
-  pivot_wider(.,names_from = "theta",values_from = c("mean","Mode","sd","rhat","upper","lower")) 
+  pivot_wider(.,names_from = "theta",values_from = c("mean","Mode","sd","upper","lower")) 
 
-f <- results[[3]] %>% mutate(ID = seq(1:100), theta = "f") %>% relocate(c("ID","theta"), .before = variable) %>% select(-variable) %>%
+f <- M3[[3]] %>% mutate(ID = seq(1:10), theta = "f") %>% relocate(c("ID","theta"), .before = variable) %>% select(-variable) %>%
   pivot_wider(.,names_from = "theta",values_from = c("mean","Mode","sd","rhat","upper","lower")) 
 
 # Recoveries of Subject Pars
 recoveries_c <- cor(parms[,"conA"],subj$mean_c)
 recoveries_a <- cor(parms[,"genA"],subj$mean_a)
-recoveries_f <- cor(parms[,"f"],f$mean_f)
+recoveries_f <- cor(parms[,"genA"],f$mean_f)
 recoveries_e <- cor(parms[,"e"],subj$mean_e)
 recoveries_r <- cor(parms[,"r"],subj$mean_r)
 
@@ -212,20 +170,20 @@ max_rhat_r <- subj %>% select(contains("rhat_r")) %>% max()
 
 
 # Stan is noisy, so tell it to be more quiet()
-M3_univ <-  mod_univ_nc_ff$sample(stan2.dat,
+M3 <-  mod3_norm$sample(stan1.dat,
                   refresh = 100,
                   chains = 4,
                   parallel_chains=4, 
                   iter_warmup=500,
-                  iter_sampling=2000,
+                  iter_sampling=1000,
                   adapt_delta=.99,
                   max_treedepth=15,
-                  init = init_univ,
+                  init=init_test,
                   show_messages = F)
 
-M3_univ$summary(c("mu_c","mu_a","mu_e","mu_r"), mean)
+M3$summary(c("hyper_pars"), mean,sd,Mode,rhat)
 
-cor(M3_univ$summary(c("r"), mean)[,2],parms[,"r"])
+cor(M3$summary(c("subj_pars[2,]"), mean)[,2],parms[,"r"])
 
 mcmc_combo(M3_univ$draws(c("mu_c","mu_a","mu_e","mu_r")))
 
@@ -233,5 +191,22 @@ mcmc_combo(M3_univ$draws(c("mu_c","mu_a","mu_e","mu_r")))
 SimDesign::RMSE(f$mean_f,parms[,"f"])
 
 
-Analyze_M3(sim3,results)
-sepe
+test <- list(stan1.dat,parms)
+
+samples<- M3[[]]
+samples[,,hyper_pars]
+
+samples <- M3$summary("hyper_pars",mean,sd,HDInterval::hdi)
+posterior::summarise_draws(samples[,,c("hyper_pars[1]","hyper_pars[2]")],mean,Mode,)
+dim(samples)
+init_test <-
+  function()
+  {
+    list(hyper_pars=cbind(runif(stan1.dat$J,10,20)),
+         subj_pars=cbind(runif(stan1.dat$N,1,10)))
+    
+  }
+
+
+a<-test[[2]][,"conA"]
+data.frame(d=a,f=test[[2]][,"conA"])
