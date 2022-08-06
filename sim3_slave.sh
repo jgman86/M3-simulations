6ecf59386b8b802ad53871b79eb42853d6206e74
jgman86 <jan.goettmann@uni-mainz.de>
2022-08-06 23:12:55 +0200

    added ramdisk allocation

--- sim3_slave.sh
+++ sim3_slave.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-#SBATCH -A m2_jgu-sim3
-#SBATCH -p parallel
-#SBATCH -C skylake
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH -c 32
-#SBATCH -t 12:00:00              # Run time (hh:mm:ss)
+#SBATCH -A m2_jgu-sim3		 # Account
+#SBATCH -p parallel 		 # Partition: parallel, smp, bigmem
+#SBATCH -C skylake 		 # architecture Skylake (64 Cores) or Broadwell (40 Cores)	
+#SBATCH -n 1                     # number of tasks
+#SBATCH -N 1			 # allocate one full node	
+#SBATCH --ramdisk=384G 		 # Reserve sufficient space for job on ramdisk	
+#SBATCH -t 02:30:00              # Run time (hh:mm:ss)
 
 
-## Move job to scratch for sufficient space
-JOBDIR="/localscratch/${SLURM_JOB_ID}"
+## Default Output 
+WD="/lustre/miifs01/project/m2_jgu-sim3/M3-simulations/"
 
+## Move job to Ramdisk for sufficient space
+JOBDIR="/localscratch/${SLURM_JOB_ID}/"
+RAMDISK=$JOBDIR/ramdisk
 
 module purge # ensures vanilla environment
 module load lang/R # will load most current version of R
 
-# for testing
-srun Rscript sim3.R -N $1 -K $2 -F $3 -R $4 -B $5 -I ${SLURM_JOB_ID} 
+cp $WD/sim3.R $RAMDISK
+cp -R $WD/Functions $RAMDISK
+cp -R $WD/Models $RAMDISK
+
+## Change Dir to Jobfolder
+cd $RAMDISK
+
+# Run Script
+srun Rscript sim3.R -N $1 -K $2 -F $3 -R $4 -P $5 -I ${SLURM_JOB_ID} -D ${WD}
+
