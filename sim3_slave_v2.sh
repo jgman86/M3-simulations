08b53626ce067ae9cdad583dcfe3a21cfc677292
jgman86 <jan.goettmann@uni-mainz.de>
2022-08-08 14:45:50 +0200

    defined tmp-dir to avaid mem problems

---
+++ sim3_slave_v2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH -A m2_jgu-sim3		 # Account
+#SBATCH -p parallel		 # Partition: parallel, smp, bigmem
+#SBATCH -C skylake 		 # architecture Skylake (64 Cores) or Broadwell (40 Cores)	
+#SBATCH -n 1                     # number of tasks
+##SBATCH -N 1
+#SBATCH -t 3:30:00              # Run time (hh:mm:ss)
+
+
+## Default Output 
+WD="/lustre/miifs01/project/m2_jgu-sim3/M3-simulations/"
+
+## Move job to Ramdisk for sufficient space
+JOBDIR="/localscratch/${SLURM_JOB_ID}/"
+TMPDIR=$JOBDIR
+
+module purge # ensures vanilla environment
+module load lang/R # will load most current version of R
+
+cp $WD/sim3.R $JOBDIR
+cp -R $WD/Functions $JOBDIR
+cp -R $WD/Models $JOBDIR
+
+## Change Dir to Jobfolder
+cd $JOBDIR
+
+# Run Script
+srun Rscript sim3.R -N $1 -K $2 -F $3 -R $4 -P $5 -I ${SLURM_JOB_ID} -D ${WD}
+
