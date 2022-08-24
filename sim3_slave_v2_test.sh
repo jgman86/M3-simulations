129f30f9ba075a5e270be250de7bae9fdc47f1e5
jgman86 <jan.goettmann@uni-mainz.de>
2022-08-24 16:46:21 +0200

    new test jobscripts

---
+++ sim3_slave_v2_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH -A m2_jgu-sim3		 # Account
+#SBATCH -p parallel		 # Partition: parallel, smp, bigmem
+##SBATCH -C skylake 		 # architecture Skylake (64 Cores) or Broadwell (40 Cores)	
+#SBATCH -c 1
+#SBATCH -n 1                     # number of tasks
+##SBATCH -N 1
+##SBATCH --mem 200G
+#SBATCH -t 00:0:30              # Run time (hh:mm:ss)
+
+
+## Default Output 
+WD="/lustre/miifs01/project/m2_jgu-sim3/M3-simulations/"
+
+## Move job to Ramdisk for sufficient space
+JOBDIR="/localscratch/${SLURM_JOB_ID}/"
+
+module purge # ensures vanilla environment
+module load lang/R # will load most current version of R
+
+cp $WD/sim3.R $JOBDIR
+cp -R $WD/Functions $JOBDIR
+cp -R $WD/Models $JOBDIR
+
+## Change Dir to Jobfolder
+cd $JOBDIR
+
+# Run Script
+srun Rscript sim3.R -N $1 -K $2 -F $3 -R $4 -I ${SLURM_JOB_ID} -D ${WD}
+
--- sim3master_test.sh
+++ sim3master_test.sh
@@ -3,8 +3,7 @@
 declare -ar nFreetime=(4)
 declare -ar nRetrievals=(1000)
 declare -ar OtherItems=(4)
-declare -ar NPL=(8)
-declare -ar fixed_f=(0)
+declare -ar NPL=(16)
 
 # submit job for every permutation
 # we keep track:
@@ -19,17 +18,14 @@ account=$(sacctmgr -n -s list user $USER format=account%30| grep -v none | head 
         for K in "${NPL[@]}"; do
            for nFT in "${nFreetime[@]}"; do
               for nRet in "${nRetrievals[@]}"; do
-		 for fixedf in "${fixed_f[@]}"; do	
-
-		    jobname="sim3_N${N}_K${K}_FT${nFT}_nRet${nRet}_fixedf${fixedf}"
+                    jobname="sim3_N${N}_K${K}_FT${nFT}_nRet${nRet}"
       		    slurmout="Jobs/${jobname}.%j.out"
                     #echo $slurmout
 		    if [[ ! -e ${slurmout} ]]; then
-        		 sbatch -A "$account" -J "$jobname" -o "$slurmout" sim3_slave.sh "$N" "$K" "$nFT" "$nRet" "$fixedf" 
+        		 sbatch -A "$account" -J "$jobname" -o "$slurmout" sim3_slave_v2_test.sh "$N" "$K" "$nFT" "$nRet" 
         		 njobs=$((njobs + 1))
 
           	     fi
-	       done
             done
 	done
     done
